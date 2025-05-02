90e229bb50bec98ab01d8ca6a54ebc3ad84286e1
yeji-gim <piggygim3@gmail.com>
2025-05-02 16:15:52 +0900

    feat: 카메라 기능 추가
    카메라 기능 추가

--- LikeZep/Assets/Scripts/Obstacle.cs
+++ LikeZep/Assets/Scripts/Obstacle.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float highPosY = 1f;
+    public float lowPosY = -1f;
 
-    // Update is called once per frame
-    void Update()
+    public float holeSizeMin = 1f;
+    public float holeSizeMax = 3f;
+
+    public Transform topObject;
+    public Transform bottomObject;
+
+    public float widthPadding = 4f;
+
+    public Vector3 SetRandomPlace(Vector3 lastPosition, int obstacleCount)
     {
-        
+        print("d");
+        float holeSize = Random.Range(holeSizeMin, holeSizeMax);
+        float halfHoleSize = holeSize / 2;
+
+        topObject.localPosition = new Vector3(0, halfHoleSize);
+        bottomObject.localPosition = new Vector3(0, -halfHoleSize);
+
+        Vector3 placePostion = lastPosition + new Vector3(widthPadding, 0);
+        placePostion.y = Random.Range(lowPosY, highPosY);
+
+        transform.position = placePostion;
+
+        return placePostion;
     }
 }
